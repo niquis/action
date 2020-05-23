1d761f4c59f41352038b26033142a14f7bf7582f
Tomas Carnecky <tomc@caurea.org>
2020-05-23 10:51:45 +0200

    ...

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,23 @@
 import * as core from "@actions/core";
-import * as https from "https";
+import * as github from "@actions/github";
 import * as fs from "fs";
+import * as https from "https";
+import fetch from "node-fetch";
 import * as path from "path";
 
 async function run(): Promise<void> {
   const time = Date.now() / 1000;
 
   try {
+    const context = github.context;
+
+    const octokit = new github.GitHub(process.env.GITHUB_TOKEN!);
+    await octokit.issues.create({
+      ...context.repo,
+      title: "New issue!",
+      body: "Hello Universe!",
+    });
+
     const workspace = process.env.GITHUB_WORKSPACE!;
     const { pages } = require(path.join(
       workspace,
@@ -21,6 +32,47 @@ async function run(): Promise<void> {
       );
       upload({ time, series: `pages${k}`, value });
     }
+
+    const res = await fetch(
+      "https://europe-west3-endless-empire-169618.cloudfunctions.net/graphql",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          query: `
+          query comparisonQuery(dataSet: String!, base: String!, head: String!) {
+            comparison(dataSet: $dataSet, base: $base, head: $head) {
+              observations {
+                id
+                series {
+                  name
+                }
+                measure {
+                  name
+                }
+                value
+                diff {
+                  base {
+                    value
+                  }
+                  absolute
+                  relative
+                }
+              }
+            }
+          }
+        `,
+          variables: {
+            dataSet: `github.com/${process.env.GITHUB_REPOSITORY!}`,
+            base: "HEAD",
+            head: process.env.GITHUB_SHA!,
+          },
+        }),
+      }
+    ).then((res) => res.json());
+    core.info(res);
   } catch (error) {
     core.setFailed(error.message);
   }
@@ -30,9 +82,9 @@ run();
 
 function upload({ time, series, value }: any) {
   const data = JSON.stringify({
-    dataSet: `github.com/${process.env.GITHUB_REPOSITORY}`,
+    dataSet: `github.com/${process.env.GITHUB_REPOSITORY!}`,
     series,
-    version: process.env.GITHUB_SHA,
+    version: process.env.GITHUB_SHA!,
     measure: "size",
     time,
     value,
