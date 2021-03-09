dc86f1a74ae65146a2a97ed7d1c2c2ac3e934ef9
Tomas Carnecky <tomc@caurea.org>
2021-03-09 21:57:43 +0100

    ...

--- src/collectors/index.ts
+++ src/collectors/index.ts
@@ -1,9 +1,9 @@
 import * as next from "./next.js";
 import * as npm from "./npm";
-import * as sloc from "./sloc";
+// import * as sloc from "./sloc";
 
 export const collectors = {
   ["next.js"]: next,
   ["npm"]: npm,
-  ["sloc"]: sloc,
+  // ["sloc"]: sloc,
 };
--- src/config.ts
+++ src/config.ts
@@ -4,5 +4,5 @@ import { collectors } from "./collectors";
 export type Config = t.TypeOf<typeof Config>;
 export const Config = t.type({
   version: t.literal("v0"),
-  collect: t.array(t.union([collectors["next.js"].Config, collectors["npm"].Config, collectors["sloc"].Config])),
+  collect: t.array(t.union([collectors["next.js"].Config, collectors["npm"].Config])),
 });
