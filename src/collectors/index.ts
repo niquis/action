2f4736a6c2216fcb9e84e87289ac193ca54b82f7
Tomas Carnecky <tomc@caurea.org>
2021-03-09 21:50:57 +0100

    ...

--- src/collectors/index.ts
+++ src/collectors/index.ts
@@ -1,7 +1,9 @@
 import * as next from "./next.js";
 import * as npm from "./npm";
+import * as sloc from "./sloc";
 
 export const collectors = {
   ["next.js"]: next,
   ["npm"]: npm,
+  ["sloc"]: sloc,
 };
---
+++ src/collectors/sloc.ts
@@ -0,0 +1,26 @@
+import * as fg from "fast-glob";
+import * as fs from "fs";
+import * as t from "io-ts";
+import * as path from "path";
+
+const sloc = require("node-sloc");
+
+export type Config = t.TypeOf<typeof Config>;
+export const Config = t.type({
+  type: t.literal("next.js"),
+  directory: t.union([t.undefined, t.string]),
+  extensions: t.union([t.undefined, t.array(t.string)]),
+});
+
+export default async function* (c: Config) {
+  const { directory = ".", extensions = ["js", "jsx", "ts", "tsx"] } = c;
+
+  const res = await sloc({
+    path: path.join(process.env.GITHUB_WORKSPACE!, directory),
+    extensions,
+  });
+
+  for (const [measure, value] of Object.entries(res.sloc)) {
+    yield { series: "sloc", measure, value };
+  }
+}
